773bfbb79bb0a9f93e298045c2b6d6d080396f6d
mnswifi <yusufabdulganiyu@outlook.com>
2025-09-06 22:15:53 +0100

    added lazy pagination

--- python-generators-0x00/1-main.py
+++ python-generators-0x00/1-main.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 
 from itertools import islice
-stream_module = __import__('0-stream_users')
-
-# Get the function from the module
-stream_users = stream_module.stream_users
+stream_users = __import__('0-stream_users').stream_users
 
 # iterate over the generator function and print only the first 6 rows
 
