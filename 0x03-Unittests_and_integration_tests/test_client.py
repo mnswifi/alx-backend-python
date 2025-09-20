a1838eeaa91b10ebba734d0e6e22c9991b1ccf45
mnswifi <yusufabdulganiyu@outlook.com>
2025-09-20 08:58:29 +0100

    implemented test has_license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -82,6 +82,16 @@ class TestGithubOrgClient(unittest.TestCase):
             # Check that public_repos returns the expected list of repo names
             self.assertEqual(result, expected_repo_names)
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test that has_license returns the expected boolean value"""
+        client = GithubOrgClient("testorg")
+        result = client.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
